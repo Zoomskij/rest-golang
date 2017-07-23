e8d3009ea88aad9fea686d25a03e78a203f48ac3
artem potapov <artem.potapov@mentalstack.com>
2017-07-23 20:47:05 +0300

    added listener

--- server.go
+++ server.go
@@ -8,5 +8,8 @@ import (
 )
 
 func main() {
-  
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  });
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
